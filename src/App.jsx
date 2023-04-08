b18541fa243df82ec8c0945d4129bafb6023dbc3
Victortxr87 <82127678+Victortxr87@users.noreply.github.com>
2023-04-08 15:21:23 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import './App.css'
+import ListaDeNotas from './Components/ListaDeNotas/';
+import  FormularioCadastro from './Components/FormularioCadastro/';
+
+class App extends Component {
+
+
+
+constructor(){
+  super()
+
+  this.state = {
+    notas:[]
+  }
+  
+}
+
+
+
+  criarNota(titulo, texto){
+
+   const novaNota = {titulo , texto};
+   const novoArrayNotas = [...this.state.notas, novaNota]
+   const novoEstado = {
+
+    notas:novoArrayNotas
+   }
+
+   
+   this.setState(novoEstado)
+ 
+
+
+  }
+  render() {
+
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
+      </section>
+    );
+  }
+}
+
+export default App;
